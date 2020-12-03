8858a48e4e3b910e6f11ab4ac99d7359b4a4e579
Bo Yuan <boyuan@g.harvard.edu>
2020-12-02 22:08:14 -0800

    Update query.py

--- maindb/query.py
+++ maindb/query.py
@@ -6,6 +6,8 @@ path = "/static/img"
 
 def get_pil_array(img_arr, res=32):
     img = np.array(img_arr)
+    assert len(img.shape) == 2, img.shape
+
     img = img.reshape(img.shape[0], img.shape[1], 3)
     return img
 
