4ef8cdd451fcee787634ea93701294463dd5016f
Bo Yuan <boyuan@g.harvard.edu>
2020-12-07 18:22:53 -0800

    better frontend

--- maindb/query.py
+++ maindb/query.py
@@ -45,7 +45,7 @@ def magenta_predict(magenta_model, img_content, img_style, weight):
     assert 0<=weight<=1
 
     outfns = magenta_model.run("/static/imgs/", [weight])
-    outfns = content_images_paths[0], outfns[0], style_images_paths[0]
+    outfns = style_images_paths[0], content_images_paths[0], outfns[0]
 
     return outfns
 
